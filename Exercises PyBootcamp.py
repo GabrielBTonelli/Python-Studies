aaa1922a3861aa56812f733a58c3aa6938cd2460
Gabriel Tonelli <tonelli.gabriel@gmail.com>
2022-10-23 20:44:15 +0200

    add Python Bootcamp Studies

---
+++ Exercises PyBootcamp.py
@@ -0,0 +1,50 @@
+#----------------------------------------------Day two
+
+# two_digit_number = input("Type a two digit number: ")
+# convert = str(two_digit_number)
+# soma = (int(convert[0]) + int(convert[1]))
+# print(soma)
+# ___________________________________________________________________________________________________________________
+
+
+#_______________________________________________________________________________________________________BMI Calculator
+
+# height = input("enter your height in m: ")
+# weight = input("enter your weight in kg: ")
+#
+# altura = float(height)
+# peso = float(weight)
+# BMI = (peso / (altura * altura))
+#
+# print(int(BMI))
+# ____________________________________________________________________________________________________________________
+
+
+#-------------------------------------------------------- How many Years, Weeks and Months do you still have in Earth?
+# age = input("What is your current age?")
+#
+# age_as_int = int(age)
+# years_remaining = 90 - age_as_int
+# days_remaining = years_remaining * 365
+# weeks_remaining = years_remaining * 52
+# months_remaining = years_remaining * 12
+#
+# print(f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left.")
+# --------------------------------------------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------------------------------TIP CALCULATOR
+# print("Hello, there! Try this tip calculator!")
+# bill = float(input("What was the total bill? $"))
+# tip = int(input("How much tip would you like to give? "))
+# people = int(input("How many people to split the bill?"))
+#
+# tip_as_percent = tip / 100
+# total_tip_amount = bill * tip_as_percent
+# total_bill = bill + total_tip_amount
+# bill_per_person = total_bill / people
+# final_amount = round(bill_per_person, 2)
+#
+# print(f"Each person should pay: {final_amount}")
+# --------------------------------------------------------------------------------------------------------------------
+
---
+++ day1_15.py
@@ -0,0 +1,26 @@
+# 100_days_Python Bootcamp
+#
+#
+# print("Hello world!\nHello World!")
+
+# _____________________________________
+#------------ Data Types --------------
+
+# String - "Hello"
+# Interger - 123456789
+# Float - 3.14159 or 3141.59
+# Boolean - True or False
+
+# type() - checks the type of the statement
+# _____________________________________
+
+# if you want to ROUND any number, use ex.: round(8 / 3, 2)
+# the number 2 after the , means in how many decimals you want it rounded.
+
+#______________f-string ALLOWS YOU TO MIX THE ENTRY TYPES ON A STRING USING f RIGHT BEFORE THE "
+# ex.:
+# score = 0
+# height = 1.8
+# isWinning = True
+# print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
+

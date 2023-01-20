1894b69d0540e2ad19738b25187176f00b78dd23
Gabriel Tonelli <tonelli.gabriel@gmail.com>
2023-01-20 11:31:23 +0100

    Day 4

--- Exercises PyBootcamp.py
+++ Exercises PyBootcamp.py
@@ -4,7 +4,7 @@
 # convert = str(two_digit_number)
 # soma = (int(convert[0]) + int(convert[1]))
 # print(soma)
-# ___________________________________________________________________________________________________________________
+# ______________________________________________________________________________________________________
 
 
 #_______________________________________________________________________________________________________BMI Calculator
@@ -17,7 +17,7 @@
 # BMI = (peso / (altura * altura))
 #
 # print(int(BMI))
-# ____________________________________________________________________________________________________________________
+# _______________________________________________________
 
 
 #-------------------------------------------------------- How many Years, Weeks and Months do you still have in Earth?
@@ -30,7 +30,7 @@
 # months_remaining = years_remaining * 12
 #
 # print(f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left.")
-# --------------------------------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------------
 
 
 # ------------------------------------------------------------------------------------------------------TIP CALCULATOR
@@ -46,5 +46,55 @@
 # final_amount = round(bill_per_person, 2)
 #
 # print(f"Each person should pay: {final_amount}")
-# --------------------------------------------------------------------------------------------------------------------
-
+# --------------------------------------------------------------------------------------------------
+#
+#
+# -------------------------------------------------------------------------------------------------ODD OR EVEN NUMBERS
+#number = int(input("Which number do you want to check? "))
+#
+#if number % 2 == 0:
+#    print(f"Your number is {number} and it's an EVEN NUMBER!")
+#else:
+#    print(f"{number} is an ODD NUMBER!")
+#---------------------------------------------------------------------------------------------------------
+#
+#---------------------------------------------------------------------------------------------------------BMI Calculator
+#height = float(input("enter your height in m: "))
+#weight = float(input("enter your weight in kg: "))
+#
+#bmi = round(weight / (height * height))
+#
+#if bmi < 18.5:
+#    print(f"Your BMI is {bmi}, you are underweight.")
+#elif bmi < 25:
+#    print(f"Your BMI is {bmi}, you have a normal weight.")
+#elif bmi < 30:
+#    print(f"Your BMI is {bmi}, you are slightly overweight.")
+#elif bmi < 35:
+#    print(f"Your BMI is {bmi}, you are obese.")
+#else:
+#    print(f"Your BMI is {bmi}, you are clinically obese.")
+#_____________________________________________________________________________________________________
+#
+#-----------------------------------------------------------------------------------------------------Is it a leap year?
+#
+#year = int(input("Which year do you want to check? "))
+#
+#if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+#    print("Leap year.")
+#else:
+#    print("Not leap year.")
+#---------------------------------------------------------------------------------------
+#
+#----------------------------------------------------------------------------------------------DAY FOUR
+#                                 RANDOMISATION
+#
+# it's a module and you have to import it to use with: 
+# import random
+#
+# ex.: random_interger = random.randit(1, 10)
+#     print(random_interger)
+#
+# you'll get a different random number between 1 and 10 every time you run it.
+#
+#
--- day1_15.py
+++ day1_15.py
@@ -17,10 +17,47 @@
 # if you want to ROUND any number, use ex.: round(8 / 3, 2)
 # the number 2 after the , means in how many decimals you want it rounded.
 
-#______________f-string ALLOWS YOU TO MIX THE ENTRY TYPES ON A STRING USING f RIGHT BEFORE THE "
+
+
+#CLASS02_________________f-string ALLOWS YOU TO MIX THE ENTRY TYPES ON A STRING USING f RIGHT BEFORE THE "
 # ex.:
 # score = 0
 # height = 1.8
 # isWinning = True
 # print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
 
+
+
+#CLASS03____________________________________________________Flow with IF / ELSE and Conditional Operations
+#
+# if CONDITION:
+#    DO THIS
+# else:
+#    DO THIS
+#
+#
+# ------NESTED IF / ELSE---------
+# for two conditions inside one condition
+#
+# if CONDITION:
+#   DO THIS
+#        if CONDITION:
+#           DO THIS
+#        else:
+#           DO THIS
+# else:
+#   DO THIS
+#
+#-------- ELIF ------------
+# for three or more conditions inside one condition
+#
+# if CONDITION 1:
+#   DO A
+# elif CONDITION 2:
+#   DO B
+# elif CONDITION 3:
+#   DO C
+# else:
+#   DO THIS
+#
+#----------------------------------------------------------------------------------------------------------------------
